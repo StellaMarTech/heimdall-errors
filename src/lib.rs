cd192627014f3a5a635db0a3c2d835a7a4f8cc3a
jonhteper <jonhteper@protonmail.com>
2022-07-06 18:08:11 -0500

    feat: new macro that recovery ErrorKinds

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Macro for auto impl From errors
+//! Macros for auto impl [From<T>] for errors
 
 /// Generate the [From<T>] trait implementation for an custom error struct with an specific
 /// structure, with ErrorKind, and message attributes;
@@ -76,6 +76,88 @@ macro_rules! implement_error {
             fn from(error: $t) -> $err {
                 $err {
                     kind: $kind,
+                    message: error.to_string(),
+                }
+            }
+        }
+    };
+}
+
+/// Generate the [From<T>] trait implementation for an custom error struct with an specific
+/// structure, with ErrorKind, and message attributes. Use only if you want recovery the ErrorKind.
+///
+/// **Warning**: This macro use the `kind()` method. Make sure that the error implemented this method.
+///
+/// * Param 1: the custom error type,
+/// * Param 2: the error type you wants use in From trait,
+/// * Param 3: the corresponding ErrorKind.
+///
+/// # Example
+/// ```
+/// use std::fmt::{Display, Formatter, Result, Debug};
+/// use std::io::Error;
+/// use heimdall_errors::implement_error_with_kind;
+///
+/// // First, create your ErrorKind
+///#[derive(Debug, PartialEq, Clone)]
+/// pub (crate) enum ErrorKind {
+///     Io(std::io::ErrorKind),
+/// }
+///
+/// impl ToString for ErrorKind {
+///     fn to_string(&self) -> String {
+///         format!("{:?}", &self)
+///     }
+/// }
+///
+/// // Next, create your Error struct
+/// #[derive(Debug, PartialEq, Clone)]
+/// pub (crate) struct MyErrorType {
+///     kind: ErrorKind,
+///     message: String
+/// }
+///
+/// // Implement the Display trait
+/// impl Display for MyErrorType {
+///     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+///         write!(
+///             f,
+///             "kind: {} message: {}",
+///             self.kind.to_string(),
+///             self.message
+///         )
+///     }
+/// }
+///
+/// // Generate implementation
+/// implement_error_with_kind!(MyErrorType, std::io::Error, ErrorKind::Io);
+/// ```
+///
+///# Code generated
+/// The code
+/// ```ignore
+/// implement_error_with_kind!(MyErrorType, std::io::Error, ErrorKind::Io);
+/// ```
+///
+/// generates the next code
+///
+///```ignore
+/// impl From<std::io::Error> for MyErrorType {
+///    fn from(err: std::io::Error) -> Self {
+///        Self {
+///            kind: ErrorKind::Io(err.kind()),
+///            message: err.to_string(),
+///        }
+///     }
+/// }
+/// ```
+#[macro_export]
+macro_rules! implement_error_with_kind {
+    ($err:ident, $t: path, $kind: path) => {
+        impl From<$t> for $err {
+            fn from(error: $t) -> $err {
+                $err {
+                    kind: $kind(error.kind().clone()),
                     message: error.to_string(),
                 }
             }
